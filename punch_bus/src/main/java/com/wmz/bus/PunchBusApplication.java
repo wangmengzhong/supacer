1d8020d39ef5b8f4536b5b35e67155bcc01fb939
wangmengzhong <2369915949@qq.com>
2018-11-29 20:03:44 +0800

    bus

---
+++ punch_bus/src/main/java/com/wmz/bus/PunchBusApplication.java
@@ -0,0 +1,12 @@
+package com.wmz.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunchBusApplication.class, args);
+	}
+}
