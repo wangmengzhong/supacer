9c0803f15c6118914d577655e6574b68f94f701d
wangmengzhong <2369915949@qq.com>
2018-11-29 19:56:59 +0800

    config

---
+++ punch_config/src/main/java/com/wmz/punch_config/PunchConfigApplication.java
@@ -0,0 +1,12 @@
+package com.wmz.punch_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunchConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunchConfigApplication.class, args);
+	}
+}
