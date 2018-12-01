d9245737b15a84ccf045de5b8eca0c14de41ea0c
wangmengzhong <2369915949@qq.com>
2018-12-01 14:10:09 +0800

    ss

---
+++ punch_zuul/src/main/java/com/wmz/punch_zuul/filter/PostFilter.java
@@ -0,0 +1,37 @@
+package com.wmz.punch_zuul.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class PostFilter extends ZuulFilter{
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+ 
+		System.out.println("------- PostFilter run --------");
+		
+		return null;
+	}
+ 
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+ 
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+ 
+	@Override
+	public String filterType() {
+		return "post";// 在请求被处理之后，会进入该过滤器
+	}
+
+}
