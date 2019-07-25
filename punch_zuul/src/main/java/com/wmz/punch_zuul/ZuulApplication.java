bbd849382341964bdb17e7f63281282f8c237311
86188 <86188@LAPTOP-2IEP3TFM>
2019-07-25 17:21:03 +0800

    zull

--- punch_zuul/src/main/java/com/wmz/punch_zuul/ZuulApplication.java
+++ punch_zuul/src/main/java/com/wmz/punch_zuul/ZuulApplication.java
@@ -11,10 +11,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy 
 @SpringBootApplication
-public class PunchZuulApplication {
+public class ZuulApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PunchZuulApplication.class, args);
+		SpringApplication.run(ZuulApplication.class, args);
 	}
 	
 	
